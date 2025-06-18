57f4d60cf0cb7b784dc09f25d8f2daa49fc23b5b
sabuchn <24261043@kyoto-wu.ac.jp>
2025-06-18 11:20:51 +0900

    Add files via upload

---
+++ no2.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import qrcode
+import io
+
+st.title("URLをQRcordeに変換")
+
+with st.form(key="url-input",clear_on_submit=True):
+    url=st.text_input("URL:")
+    button=st.form_submit_button("変換")
+
+if  url:
+    img=qrcode.make(url)
+    with io.BytesIO() as f:
+        img.save(f,format="PNG")
+        png=f.getvalue()
+    st.write(url)
+    st.image(png)
+    st.download_button("Download",data=png,file_name="urlqr.png")
